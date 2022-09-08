11f8ef5ae432c270868284d25fbf3f331fe1b26e
Ivo <ivorosenberger@gmail.com>
2022-09-08 19:41:40 -0300

    agregado de ultimos ej

---
+++ practicaTs/Ejercicio1.1.js
@@ -0,0 +1,3 @@
+//1 Usando tu IDE preferido, coloca el cursor sobre las errores en rojo para inspeccionarlos
+//2 Coloca el cursor sobre las variables para inspeccionar sus tipos
+//3 Arregle el error en la linea 2 cambiando el valor de pi al tipo esperado
---
+++ practicaTs/Ejercicio1.2.js
@@ -0,0 +1,8 @@
+/*Ejercicio 1.2
+Dado el siguiente codigo:
+1 Inspeccionar el tipo de ‘torta‘
+2 Añadir una anotación de tipo explícito a ‘torta‘
+3 Intenta asignar tipos inválidos, por diversión*/
+var torta;
+torta = 'arandanos';
+console.log('[Ejercicio 1.2]', "Me gusta comer torta con sabor a ".concat(torta, "."));
